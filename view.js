98a2f9a905e6d12ca3c80ff737f361247745daf3
Andrew Beers <andybeers@gmail.com>
2017-02-08 20:14:35 -0800

    questions added

--- view.js
+++ view.js
@@ -1,3 +1,8 @@
 $(function() {
-  $('#question2').hide();
+  $('#qsubmit').on('click', function() {
+    $('#%').text('120%');
+    $('#question1').fadeOut();
+    $('#score').fadeIn();
+  });
 });
+
